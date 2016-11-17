4c0e4519a349623bc62083886b805dce33aa876f
Chester Beard <buzzuw2@gmail.com>
2016-11-17 09:25:03 -0800

    courses

---
+++ djangoEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chesterbeard/Desktop/CodingDojo_Django/Django2016/courses/djangoEnv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
